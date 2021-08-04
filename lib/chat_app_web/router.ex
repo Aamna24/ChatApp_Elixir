c702040bccd6a7a261fe987a51f2327971680034
aamna <amna.majid@invozone.com>
2021-08-04 18:39:16 +0500

    authenticate user

--- lib/chat_app_web/router.ex
+++ lib/chat_app_web/router.ex
@@ -7,6 +7,7 @@ defmodule ChatAppWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+
   end
 
   pipeline :api do
@@ -14,9 +15,39 @@ defmodule ChatAppWeb.Router do
   end
 
   scope "/", ChatAppWeb do
-    pipe_through :browser
-
+    pipe_through [:browser , :authenticate_user, :put_user_token]
     get "/", PageController, :index
+
+  end
+
+  scope "/", ChatAppWeb do
+    pipe_through :browser
+    #get "/", PageController, :index
+    resources "/users", UserController
+
+    resources "/sessions", SessionController, only: [:new, :create, :delete],
+                                              singleton: true
+  end
+
+  defp put_user_token(conn, _) do
+    if current_user = conn.assigns[:current_user] do
+      token = Phoenix.Token.sign(conn, "user socket", current_user.id)
+      assign(conn, :user_token, token)
+    else
+      conn
+    end
+  end
+
+  defp authenticate_user(conn, _) do
+    case get_session(conn, :user_id) do
+      nil ->
+        conn
+        |> Phoenix.Controller.put_flash(:error, "Login required")
+        |> Phoenix.Controller.redirect(to: "/users")
+        |> halt()
+      user_id ->
+        assign(conn, :current_user, ChatApp.Accounts.get_user!(user_id))
+    end
   end
 
   # Other scopes may use custom stacks.
